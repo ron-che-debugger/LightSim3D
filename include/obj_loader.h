bc90103ba77bfb547eef20891ffa42eef183634d
ron-che-debugger <ron.cherh63@gmail.com>
2025-03-15 13:01:45 -0400

    Functions to load OBJs

---
+++ include/obj_loader.h
@@ -0,0 +1,16 @@
+#ifndef OBJ_LOADER_H
+#define OBJ_LOADER_H
+
+#include "tiny_obj_loader.h"
+#include <vector>
+
+using namespace std;
+
+struct Triangle {
+    float3 v0, v1, v2;
+    float3 normal;
+};
+
+bool loadOBJ(const string &filename, vector<Triangle> &triangles);
+
+#endif
